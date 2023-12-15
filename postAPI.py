7365d09c5af6300858d232acc954c252e33630b5
Shaheer334 <shaheerzeb1@gmail.com>
2023-12-16 04:05:57 +0500

    cr

--- postAPI.py
+++ postAPI.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 import requests
 app = Flask(__name__)
-# Replace 'NODE_SERVER_URL' with the actual URL of your Node.js server
 NODE_SERVER_URL = 'http://localhost:3000'
 @app.route('/api/school', methods=['POST'])
 def create_or_update_school():
